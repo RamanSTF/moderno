d601e51b1d5713ee675f57530a4f045294f05ba0
RamanSTF <plchsnik@gmail.com>
2023-02-12 18:21:09 +0100

    added advantages

--- app/js/main.js
+++ app/js/main.js
@@ -2,21 +2,22 @@ $(function(){
 
 
 
-
 var mixer = mixitup('.products__inner-box');
 
-$(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true
-  });
-
 $(".product-slider__inner").slick({
-    dots: true,
-    arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4
+  dots: true,
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 4
 });
+
+$(".rate-star").rateYo({
+  rating: 5,
+  starWidth: "12px",
+  readOnly: true
+});
+
+
 
 
 });
